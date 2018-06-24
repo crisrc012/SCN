12e9cba81f0ccb626a87fe0cd4d08095b08fa589
Cristopher Robles <crisrc012@hotmail.com>
2018-06-24 16:03:16 -0600

    Validación de contraseña con ErrorProvider

--- SCN/PL_SCN/frmIniciarSesion.Designer.cs
+++ SCN/PL_SCN/frmIniciarSesion.Designer.cs
@@ -46,9 +46,10 @@
             this.btnEntrar.ForeColor = System.Drawing.Color.Green;
             this.btnEntrar.Image = ((System.Drawing.Image)(resources.GetObject("btnEntrar.Image")));
             this.btnEntrar.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.btnEntrar.Location = new System.Drawing.Point(267, 141);
+            this.btnEntrar.Location = new System.Drawing.Point(356, 184);
+            this.btnEntrar.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnEntrar.Name = "btnEntrar";
-            this.btnEntrar.Size = new System.Drawing.Size(99, 25);
+            this.btnEntrar.Size = new System.Drawing.Size(132, 33);
             this.btnEntrar.TabIndex = 0;
             this.btnEntrar.Text = "Entrar";
             this.btnEntrar.UseVisualStyleBackColor = true;
@@ -61,18 +62,21 @@
             this.btnSalir.ForeColor = System.Drawing.Color.Red;
             this.btnSalir.Image = ((System.Drawing.Image)(resources.GetObject("btnSalir.Image")));
             this.btnSalir.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.btnSalir.Location = new System.Drawing.Point(12, 190);
+            this.btnSalir.Location = new System.Drawing.Point(16, 248);
+            this.btnSalir.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnSalir.Name = "btnSalir";
-            this.btnSalir.Size = new System.Drawing.Size(99, 25);
+            this.btnSalir.Size = new System.Drawing.Size(132, 33);
             this.btnSalir.TabIndex = 1;
             this.btnSalir.Text = "Salir";
             this.btnSalir.UseVisualStyleBackColor = true;
+            this.btnSalir.Click += new System.EventHandler(this.btnSalir_Click);
             // 
             // lblUsuario
             // 
             this.lblUsuario.AutoSize = true;
             this.lblUsuario.Font = new System.Drawing.Font("Segoe UI Semibold", 9.75F, System.Drawing.FontStyle.Bold);
-            this.lblUsuario.Location = new System.Drawing.Point(6, 37);
+            this.lblUsuario.Location = new System.Drawing.Point(24, 47);
+            this.lblUsuario.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblUsuario.Name = "lblUsuario";
             this.lblUsuario.Size = new System.Drawing.Size(133, 17);
             this.lblUsuario.TabIndex = 2;
@@ -82,7 +86,8 @@
             // 
             this.lblContraseña.AutoSize = true;
             this.lblContraseña.Font = new System.Drawing.Font("Segoe UI Semibold", 9.75F, System.Drawing.FontStyle.Bold);
-            this.lblContraseña.Location = new System.Drawing.Point(55, 85);
+            this.lblContraseña.Location = new System.Drawing.Point(73, 110);
+            this.lblContraseña.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblContraseña.Name = "lblContraseña";
             this.lblContraseña.Size = new System.Drawing.Size(84, 17);
             this.lblContraseña.TabIndex = 3;
@@ -96,9 +101,11 @@
             this.gbxCredenciales.Controls.Add(this.lblUsuario);
             this.gbxCredenciales.Controls.Add(this.lblContraseña);
             this.gbxCredenciales.Font = new System.Drawing.Font("Segoe UI Semibold", 9.75F, System.Drawing.FontStyle.Bold);
-            this.gbxCredenciales.Location = new System.Drawing.Point(12, 12);
+            this.gbxCredenciales.Location = new System.Drawing.Point(16, 16);
+            this.gbxCredenciales.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.gbxCredenciales.Name = "gbxCredenciales";
-            this.gbxCredenciales.Size = new System.Drawing.Size(372, 172);
+            this.gbxCredenciales.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.gbxCredenciales.Size = new System.Drawing.Size(496, 225);
             this.gbxCredenciales.TabIndex = 4;
             this.gbxCredenciales.TabStop = false;
             this.gbxCredenciales.Text = "Credenciales";
@@ -107,35 +114,41 @@
             // 
             this.txtUsuario.CharacterCasing = System.Windows.Forms.CharacterCasing.Lower;
             this.txtUsuario.Font = new System.Drawing.Font("Segoe UI Semibold", 9.75F, System.Drawing.FontStyle.Bold);
-            this.txtUsuario.Location = new System.Drawing.Point(145, 34);
+            this.txtUsuario.Location = new System.Drawing.Point(193, 44);
+            this.txtUsuario.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.txtUsuario.MaxLength = 32;
             this.txtUsuario.Name = "txtUsuario";
-            this.txtUsuario.Size = new System.Drawing.Size(202, 25);
+            this.txtUsuario.Size = new System.Drawing.Size(268, 25);
             this.txtUsuario.TabIndex = 4;
             this.txtUsuario.Validating += new System.ComponentModel.CancelEventHandler(this.txtUsuario_Validating);
             // 
             // txtContrasena
             // 
             this.txtContrasena.Font = new System.Drawing.Font("Segoe UI Semibold", 9.75F, System.Drawing.FontStyle.Bold);
-            this.txtContrasena.Location = new System.Drawing.Point(145, 82);
-            this.txtContrasena.MaxLength = 32;
+            this.txtContrasena.Location = new System.Drawing.Point(193, 107);
+            this.txtContrasena.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtContrasena.MaxLength = 26;
             this.txtContrasena.Name = "txtContrasena";
             this.txtContrasena.PasswordChar = '*';
-            this.txtContrasena.Size = new System.Drawing.Size(202, 25);
+            this.txtContrasena.Size = new System.Drawing.Size(268, 25);
             this.txtContrasena.TabIndex = 5;
+            this.txtContrasena.Validating += new System.ComponentModel.CancelEventHandler(this.txtContrasena_Validating);
             // 
             // frmIniciarSesion
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 17F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(396, 224);
+            this.ClientSize = new System.Drawing.Size(528, 293);
             this.Controls.Add(this.gbxCredenciales);
             this.Controls.Add(this.btnSalir);
+            this.Font = new System.Drawing.Font("Segoe UI Semibold", 9.75F, System.Drawing.FontStyle.Bold);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "frmIniciarSesion";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "SCN - Iniciar Sesión";
             this.gbxCredenciales.ResumeLayout(false);
             this.gbxCredenciales.PerformLayout();
--- SCN/PL_SCN/frmIniciarSesion.cs
+++ SCN/PL_SCN/frmIniciarSesion.cs
@@ -1,11 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace PL_SCN
@@ -25,7 +19,6 @@ namespace PL_SCN
             if (string.IsNullOrEmpty(txtUsuario.Text.Trim()))
             {
                 e.Cancel = true;
-                txtUsuario.Focus();
                 errorProvider.SetError(txtUsuario, "Por favor digite su nombre de usuario.");
             }
             else
@@ -34,15 +27,55 @@ namespace PL_SCN
                 errorProvider.SetError(txtUsuario, null);
             }
         }
-
+        private void txtContrasena_Validating(object sender, CancelEventArgs e)
+        {
+            if (string.IsNullOrEmpty(txtContrasena.Text.Trim()))
+            {
+                e.Cancel = true;
+                errorProvider.SetError(txtContrasena,
+                    "Por favor digite su contraseña.");
+            }
+            else if (txtContrasena.Text.Trim().Length < 6)
+            {
+                e.Cancel = true;
+                errorProvider.SetError(txtContrasena,
+                    "La contraseña debe de tener al menos 6 caracteres.");
+            }
+            else if (txtContrasena.Text.Trim().Length > 25)
+            {
+                e.Cancel = true;
+                errorProvider.SetError(txtContrasena,
+                    "La contraseña no puede más de 25 caracteres.");
+            }
+            else
+            {
+                e.Cancel = false;
+                errorProvider.SetError(txtContrasena, null);
+            }
+        }
         private void btnEntrar_Click(object sender, EventArgs e)
         {
             if (ValidateChildren(ValidationConstraints.Enabled))
             {
-                Visible = false;
-                frmSCNInstancia = new frmSCN();
-                frmSCNInstancia.Show();
+                if (true)
+                {
+                    // Si autentica
+                    Visible = false;
+                    frmSCNInstancia = new frmSCN();
+                    frmSCNInstancia.Show();
+                }
+                else
+                {
+                    MessageBox.Show("Usuario o contraseña inválido",
+                        "Error de inicio de sesión", MessageBoxButtons.OK,
+                        MessageBoxIcon.Error);
+                }
             }
+        }
+
+        private void btnSalir_Click(object sender, EventArgs e)
+        {
+            Close();
         }
     }
 }
