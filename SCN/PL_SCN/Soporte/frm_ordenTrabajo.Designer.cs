0d4820b76ef37a5ab07d5e746771c0f32d1b3419
carlosgn9090 <carlosgn90@hotmailcom>
2018-06-27 14:16:52 -0600

    Ordenes de Trabajo

--- SCN/PL_SCN/Soporte/frm_ordenTrabajo.Designer.cs
+++ SCN/PL_SCN/Soporte/frm_ordenTrabajo.Designer.cs
@@ -55,7 +55,9 @@
             this.reportesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.nuevoReporteToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.generarReporteToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
             this.salirF5ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.label5 = new System.Windows.Forms.Label();
             this.mmMenuOrdenTrabajo.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -63,7 +65,7 @@
             // 
             this.textBox1.Enabled = false;
             this.textBox1.Font = new System.Drawing.Font("Segoe UI Semibold", 9.75F, System.Drawing.FontStyle.Bold);
-            this.textBox1.Location = new System.Drawing.Point(657, 34);
+            this.textBox1.Location = new System.Drawing.Point(657, 44);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(100, 25);
             this.textBox1.TabIndex = 0;
@@ -71,8 +73,9 @@
             // label1
             // 
             this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
             this.label1.Font = new System.Drawing.Font("Segoe UI Semibold", 9.75F, System.Drawing.FontStyle.Bold);
-            this.label1.Location = new System.Drawing.Point(532, 37);
+            this.label1.Location = new System.Drawing.Point(532, 47);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(122, 17);
             this.label1.TabIndex = 1;
@@ -81,8 +84,9 @@
             // label2
             // 
             this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
             this.label2.Font = new System.Drawing.Font("Segoe UI Semibold", 9.75F, System.Drawing.FontStyle.Bold);
-            this.label2.Location = new System.Drawing.Point(12, 37);
+            this.label2.Location = new System.Drawing.Point(12, 47);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(125, 17);
             this.label2.TabIndex = 3;
@@ -91,7 +95,7 @@
             // textBox2
             // 
             this.textBox2.Font = new System.Drawing.Font("Segoe UI Semibold", 9.75F, System.Drawing.FontStyle.Bold);
-            this.textBox2.Location = new System.Drawing.Point(137, 34);
+            this.textBox2.Location = new System.Drawing.Point(137, 44);
             this.textBox2.Name = "textBox2";
             this.textBox2.Size = new System.Drawing.Size(130, 25);
             this.textBox2.TabIndex = 2;
@@ -99,6 +103,7 @@
             // label3
             // 
             this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
             this.label3.Font = new System.Drawing.Font("Segoe UI Semibold", 9.75F, System.Drawing.FontStyle.Bold);
             this.label3.Location = new System.Drawing.Point(12, 98);
             this.label3.Name = "label3";
@@ -118,8 +123,9 @@
             // label4
             // 
             this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Transparent;
             this.label4.Font = new System.Drawing.Font("Segoe UI Semibold", 9.75F, System.Drawing.FontStyle.Bold);
-            this.label4.Location = new System.Drawing.Point(312, 34);
+            this.label4.Location = new System.Drawing.Point(312, 44);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(46, 17);
             this.label4.TabIndex = 7;
@@ -128,8 +134,9 @@
             // lbl_fechaActual
             // 
             this.lbl_fechaActual.AutoSize = true;
+            this.lbl_fechaActual.BackColor = System.Drawing.Color.Transparent;
             this.lbl_fechaActual.Font = new System.Drawing.Font("Segoe UI Semibold", 9.75F, System.Drawing.FontStyle.Bold);
-            this.lbl_fechaActual.Location = new System.Drawing.Point(364, 34);
+            this.lbl_fechaActual.Location = new System.Drawing.Point(364, 44);
             this.lbl_fechaActual.Name = "lbl_fechaActual";
             this.lbl_fechaActual.Size = new System.Drawing.Size(82, 17);
             this.lbl_fechaActual.TabIndex = 8;
@@ -138,6 +145,7 @@
             // label6
             // 
             this.label6.AutoSize = true;
+            this.label6.BackColor = System.Drawing.Color.Transparent;
             this.label6.Font = new System.Drawing.Font("Segoe UI Semibold", 9.75F, System.Drawing.FontStyle.Bold);
             this.label6.Location = new System.Drawing.Point(12, 163);
             this.label6.Name = "label6";
@@ -157,6 +165,7 @@
             // label7
             // 
             this.label7.AutoSize = true;
+            this.label7.BackColor = System.Drawing.Color.Transparent;
             this.label7.Font = new System.Drawing.Font("Segoe UI Semibold", 9.75F, System.Drawing.FontStyle.Bold);
             this.label7.Location = new System.Drawing.Point(358, 98);
             this.label7.Name = "label7";
@@ -167,8 +176,9 @@
             // label8
             // 
             this.label8.AutoSize = true;
+            this.label8.BackColor = System.Drawing.Color.Transparent;
             this.label8.Font = new System.Drawing.Font("Segoe UI Semibold", 9.75F, System.Drawing.FontStyle.Bold);
-            this.label8.Location = new System.Drawing.Point(-3, 60);
+            this.label8.Location = new System.Drawing.Point(-3, 68);
             this.label8.Name = "label8";
             this.label8.Size = new System.Drawing.Size(798, 17);
             this.label8.TabIndex = 12;
@@ -189,6 +199,7 @@
             // label9
             // 
             this.label9.AutoSize = true;
+            this.label9.BackColor = System.Drawing.Color.Transparent;
             this.label9.Font = new System.Drawing.Font("Segoe UI Semibold", 9.75F, System.Drawing.FontStyle.Bold);
             this.label9.Location = new System.Drawing.Point(-6, 261);
             this.label9.Name = "label9";
@@ -200,6 +211,7 @@
             // label10
             // 
             this.label10.AutoSize = true;
+            this.label10.BackColor = System.Drawing.Color.Transparent;
             this.label10.Font = new System.Drawing.Font("Segoe UI Semibold", 9.75F, System.Drawing.FontStyle.Bold);
             this.label10.Location = new System.Drawing.Point(12, 295);
             this.label10.Name = "label10";
@@ -220,6 +232,7 @@
             // label11
             // 
             this.label11.AutoSize = true;
+            this.label11.BackColor = System.Drawing.Color.Transparent;
             this.label11.Font = new System.Drawing.Font("Segoe UI Semibold", 9.75F, System.Drawing.FontStyle.Bold);
             this.label11.Location = new System.Drawing.Point(382, 297);
             this.label11.Name = "label11";
@@ -239,6 +252,7 @@
             // label12
             // 
             this.label12.AutoSize = true;
+            this.label12.BackColor = System.Drawing.Color.Transparent;
             this.label12.Font = new System.Drawing.Font("Segoe UI Semibold", 9.75F, System.Drawing.FontStyle.Bold);
             this.label12.Location = new System.Drawing.Point(384, 340);
             this.label12.Name = "label12";
@@ -259,6 +273,10 @@
             // 
             this.comboBox2.Font = new System.Drawing.Font("Segoe UI Semibold", 9.75F, System.Drawing.FontStyle.Bold);
             this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] {
+            "Alta",
+            "Media",
+            "Baja"});
             this.comboBox2.Location = new System.Drawing.Point(490, 381);
             this.comboBox2.Name = "comboBox2";
             this.comboBox2.Size = new System.Drawing.Size(121, 25);
@@ -267,6 +285,7 @@
             // label13
             // 
             this.label13.AutoSize = true;
+            this.label13.BackColor = System.Drawing.Color.Transparent;
             this.label13.Font = new System.Drawing.Font("Segoe UI Semibold", 9.75F, System.Drawing.FontStyle.Bold);
             this.label13.Location = new System.Drawing.Point(382, 383);
             this.label13.Name = "label13";
@@ -276,9 +295,11 @@
             // 
             // mmMenuOrdenTrabajo
             // 
+            this.mmMenuOrdenTrabajo.BackColor = System.Drawing.Color.Transparent;
             this.mmMenuOrdenTrabajo.Font = new System.Drawing.Font("Segoe UI Semibold", 9.75F, System.Drawing.FontStyle.Bold);
             this.mmMenuOrdenTrabajo.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.reportesToolStripMenuItem,
+            this.toolStripMenuItem1,
             this.salirF5ToolStripMenuItem});
             this.mmMenuOrdenTrabajo.Location = new System.Drawing.Point(0, 0);
             this.mmMenuOrdenTrabajo.Name = "mmMenuOrdenTrabajo";
@@ -307,17 +328,38 @@
             this.generarReporteToolStripMenuItem.Size = new System.Drawing.Size(203, 22);
             this.generarReporteToolStripMenuItem.Text = "Generar Reporte - F2";
             // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(24, 21);
+            this.toolStripMenuItem1.Text = "|";
+            // 
             // salirF5ToolStripMenuItem
             // 
             this.salirF5ToolStripMenuItem.Name = "salirF5ToolStripMenuItem";
             this.salirF5ToolStripMenuItem.Size = new System.Drawing.Size(72, 21);
             this.salirF5ToolStripMenuItem.Text = "Salir - F5";
             // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.Color.Transparent;
+            this.label5.Font = new System.Drawing.Font("Segoe UI Semibold", 9.75F, System.Drawing.FontStyle.Bold);
+            this.label5.Location = new System.Drawing.Point(-10, 21);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(798, 17);
+            this.label5.TabIndex = 24;
+            this.label5.Text = "_________________________________________________________________________________" +
+    "_____________________________________________________________________________";
+            // 
             // frm_ordenTrabajo
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::PL_SCN.Properties.Resources.fondo_2;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.ClientSize = new System.Drawing.Size(787, 437);
+            this.Controls.Add(this.label5);
             this.Controls.Add(this.comboBox2);
             this.Controls.Add(this.label13);
             this.Controls.Add(this.label12);
@@ -385,5 +427,7 @@
         private System.Windows.Forms.ToolStripMenuItem nuevoReporteToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem generarReporteToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem salirF5ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem1;
+        private System.Windows.Forms.Label label5;
     }
 }
